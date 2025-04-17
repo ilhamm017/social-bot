63a44b4a90f6aa454036cdae22ecf18a7921c3b1
ilhamm017 <ilhammuliawan26@gmail.com>
2025-04-18 00:55:41 +0700

    feat: restructure project and add database configuration
    - Added "type": "commonjs" to package.json for module compatibility.
    - Removed unused routes and session management utilities.
    - Introduced a new config file for database connection settings.
    - Created migration for Instagram accounts table.
    - Implemented Sequelize models for Instagram accounts.
    - Added a test script to scrape quotes using Puppeteer.

---
+++ migrations/20250417142148-create-instagram-account.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Instagram_accounts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.BOOLEAN
+      },
+      cookie_path: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Instagram_accounts');
+  }
+};
